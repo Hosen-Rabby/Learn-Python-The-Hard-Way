57b167fe8c00a25afa2db46f34e1d4a0d4f4a280
Hosen-Rabby <56362824+Hosen-Rabby@users.noreply.github.com>
2021-04-21 23:57:51 +0600

    Add files via upload

---
+++ 8kyu1.py
@@ -0,0 +1,17 @@
+# Write a function to split a string and convert it into an array of words. For example:
+
+# "Robin Singh" ==> ["Robin", "Singh"]
+
+
+
+def string_to_array(s):
+    # your code here
+    
+    t = s.split()
+    print(t)
+    
+    
+s = "My name is Hosen"    
+
+string_to_array(s)
+    

728cf44b410e9e1f1eb5440917da25fb1ecba78f
Hosen-Rabby <56362824+Hosen-Rabby@users.noreply.github.com>
2021-05-01 22:48:28 +0600

    Add files via upload

---
+++ ex8.py
@@ -0,0 +1,13 @@
+formatter = "%r %r %r %r %r"
+
+print(formatter % (1, 2, 3, 4,5))
+print(formatter % ("One", "Two", "Three", "Four" , "Five")
+# print(formatter % (formatter, formatter, formatter, formatter,formatter))
+
+print(formatter % (
+	"I had this thing",
+	"That you would type up right",
+	"But it didn't sing",
+	"So I said goodnight",
+	"Wish a sound sleep"
+	))

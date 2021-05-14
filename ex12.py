2185ad02574c3cb5acdc358d4c4c44e3e4b202f4
Hosen-Rabby <56362824+Hosen-Rabby@users.noreply.github.com>
2021-05-14 10:48:52 +0600

    Update ex12.py

--- ex12.py
+++ ex12.py
@@ -1,1 +1,7 @@
-ex12.py
+y = input("Name? ")
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weight? ")
+
+
+print("So, you'are %r old, %r tall and %r heavy." %(age,height,weight))

fb28715eb6140cf1e60a9bd8d91c9caf3109b962
Hosen-Rabby <56362824+Hosen-Rabby@users.noreply.github.com>
2021-06-25 18:29:46 +0600

    Add files via upload

---
+++ access tuple items.py
@@ -0,0 +1,13 @@
+print("Can Access Tuple Items-by referring to the index number, inside square brackets")
+thistuple = ("kat", "Gari", "toy","Laptop","TV")
+print(thistuple[2])
+
+print("Negative Indexing")
+print("refers to the last item -",thistuple[-1])
+print("refers to the 2nd last item -",thistuple[-2])
+
+print("Range of Indexes")
+print(thistuple[1:])
+print(thistuple[1:6])
+print(thistuple[:5])
+print(thistuple[1:-1])
